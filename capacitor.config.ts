fc4bc955875b35753b6a2b583070b4f21af60013
DorianWrt <dorian.wrt@gmail.com>
2024-11-22 00:03:44 +0100

    connexion avec google ptn

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'EasyHair',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '104915930078-r6oq1tjevodtaahg84jdffe2t7m0gu2g.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
+  }
+
 };
 
 export default config;
