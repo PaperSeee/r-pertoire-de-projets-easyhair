05ae793f0b4dab87afef0a152ba3efa989a30145
DorianWrt <dorian.wrt@gmail.com>
2024-11-22 17:23:00 +0100

    front homepage

--- src/app/Pages/Accueil/accueil.page.ts
+++ src/app/Pages/Accueil/accueil.page.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['accueil.page.scss']
 })
 export class AccueilPage {
-  constructor() {}
+  barbers = [];
+  currentPage = 1;
+
+  constructor() {
+    // Charger les données initiales
+    this.loadBarbers();
+  }
+
+  loadBarbers(event?) {
+    // Simuler le chargement de 10 coiffeurs à la fois
+    for (let i = 0; i < 10; i++) {
+      this.barbers.push({
+        name: 'Hairmomo ' + (this.barbers.length + 1),
+        type: 'Barber professionel',
+        price: '20€-30€',
+        rating: 4.5,
+        image: '../../../assets/icon/image 4.png',
+        profile: '../../../assets/icon/pp.png'
+      });
+    }
+
+    if (event) {
+      event.target.complete();
+    }
+  }
+
+  loadMore(event) {
+    this.currentPage++;
+    this.loadBarbers(event);
+  }
 }
