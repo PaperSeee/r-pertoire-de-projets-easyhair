f022f31f7991b872b89fcd8091cd08ece79d4736
paperseeem <ilias0703@hotmail.comm>
2024-12-10 14:59:24 +0100

    coucou

--- src/app/Pages/Client/detail-barber/detail-barber.page.ts
+++ src/app/Pages/Client/detail-barber/detail-barber.page.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FavoritesService } from '../../../services/favorites.service';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-detail-barber',
   templateUrl: './detail-barber.page.html',
   styleUrls: ['./detail-barber.page.scss']
 })
-export class DetailBarberPage implements OnInit {
+export class DetailBarberPage implements OnInit, OnDestroy {
   barber: any = {};
   isFavorite = false;
   private firestore = getFirestore();
+  private favoriteStatusSubscription?: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,34 +22,52 @@ export class DetailBarberPage implements OnInit {
   ) {}
 
   async ngOnInit() {
-    try {
-      const barberId = this.route.snapshot.paramMap.get('id');
+    this.route.paramMap.subscribe(async params => {
+      const barberId = params.get('id');
       if (barberId) {
-        const barberDoc = await getDoc(doc(this.firestore, 'Coiffeurs', barberId));
-        if (barberDoc.exists()) {
-          this.barber = {
-            id: barberDoc.id,
-            ...barberDoc.data()
-          };
-        }
+        await this.loadBarberData(barberId);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.favoriteStatusSubscription) {
+      this.favoriteStatusSubscription.unsubscribe();
+    }
+  }
+
+  private async loadBarberData(barberId: string) {
+    try {
+      const barberDoc = await getDoc(doc(this.firestore, 'Coiffeurs', barberId));
+      if (barberDoc.exists()) {
+        this.barber = {
+          id: barberDoc.id,
+          ...barberDoc.data()
+        };
+        this.subscribeToFavoriteStatus();
       }
     } catch (error) {
       console.error('Erreur lors du chargement du barbier:', error);
       this.router.navigate(['/']);
     }
-    this.checkIfFavorite();
+  }
+
+  private subscribeToFavoriteStatus() {
+    if (this.favoriteStatusSubscription) {
+      this.favoriteStatusSubscription.unsubscribe();
+    }
+    this.favoriteStatusSubscription = this.favoritesService
+      .getFavoriteStatus(this.barber.id)
+      .subscribe(status => {
+        this.isFavorite = status;
+      });
   }
 
   toggleFavorite() {
     if (this.isFavorite) {
-      this.favoritesService.removeFromFavorites(this.barber);
+      this.favoritesService.removeFromFavorites(this.barber.id);
     } else {
       this.favoritesService.addToFavorites(this.barber);
     }
-    this.isFavorite = !this.isFavorite;
-  }
-
-  private checkIfFavorite() {
-    this.isFavorite = this.favoritesService.isFavorite(this.barber);
   }
 }
