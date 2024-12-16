ffeb234ad1354d81268dcbaf668053b7437e0306
DorianWrt <dorian.wrt@gmail.com>
2024-12-16 22:38:58 +0100

    ordre

--- src/app/Pages/Coiffeur/mes-horaires/mes-horaires.page.ts
+++ src/app/Pages/Coiffeur/mes-horaires/mes-horaires.page.ts
@@ -18,6 +18,8 @@ export class MesHorairesPage implements OnInit {
   
   availableHours = ['09:00', '10:00', '11:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
   
+  orderedDays = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  
   weekSchedule: { [key: string]: DaySchedule } = {
     'Lundi': { enabled: true, hours: [...this.availableHours] },
     'Mardi': { enabled: true, hours: [...this.availableHours] },
