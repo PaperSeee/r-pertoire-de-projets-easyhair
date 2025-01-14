d4fb718be09b5c874e423b08665ed7c5cfd22372
PaperSeee <ilias300@outlook.be>
2025-01-14 15:36:27 +0100

    aa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBqqw7xytMSAJV_IRo3QPDTaFDrrLu12Ak",
+    authDomain: "easyhair-3cebd.firebaseapp.com",
+    projectId: "easyhair-3cebd",
+    storageBucket: "easyhair-3cebd.firebasestorage.app",
+    messagingSenderId: "104915930078",
+    appId: "1:104915930078:web:bdcfa5cc00bc2143086b4f",
+    measurementId: "G-8YZHMTEF0W"
+  },
+  googleClientId: '104915930078-r6oq1tjevodtaahg84jdffe2t7m0gu2g.apps.googleusercontent.com'
 };
